e4c6ccdadc22663ea9d459ec6bf973cd2abb883c
imniloy <imniloy8@gmail.com>
2023-05-03 00:59:18 +0600

    changed

--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -7,23 +7,30 @@ export const CursorContext = createContext();
 const CursorContextProvider = ({ children }) => {
   const cursorRef = useRef(null);
   const currentCursorMode = useRef("default");
+  const mobileViewportIsActive = window.innerWidth < 768;
 
   useEffect(() => {
-    const handleMouseMove = (event) => {
-      const { clientX, clientY } = event;
-      let mousePositionX = clientX - cursorRef.current.clientWidth / 2;
-      let mousePositionY = clientY - cursorRef.current.clientHeight / 2;
-      cursorRef.current.style.transform = `translate3d(${mousePositionX}px,${mousePositionY}px,0)`;
-      cursorRef.current.style.display = `inline-block`;
-    };
+    if (!mobileViewportIsActive) {
+      const handleMouseMove = (event) => {
+        const { clientX, clientY } = event;
+        let mousePositionX = clientX - cursorRef.current.clientWidth / 2;
+        let mousePositionY = clientY - cursorRef.current.clientHeight / 2;
+        cursorRef.current.style.transform = `translate3d(${mousePositionX}px,${mousePositionY}px,0)`;
+        cursorRef.current.style.display = `inline-block`;
+      };
 
-    document.addEventListener("mousemove", handleMouseMove);
-    return () => document.removeEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mousemove", handleMouseMove);
+      return () => document.removeEventListener("mousemove", handleMouseMove);
+    } else {
+      cursorRef.current.style.height = "0px";
+      cursorRef.current.style.width = "0px";
+      cursorRef.current.style.backgroundColor = "rgba(255,255,255, 1)";
+    }
   }, []);
 
   const cursorEnter = (event) => {
     currentCursorMode.current = "text";
-    if (currentCursorMode.current === "text") {
+    if (!mobileViewportIsActive && currentCursorMode.current === "text") {
       cursorRef.current.style.height = "140px";
       cursorRef.current.style.width = "140px";
       cursorRef.current.style.backgroundColor = "#fff";
@@ -36,7 +43,7 @@ const CursorContextProvider = ({ children }) => {
 
   const cursorLeave = (event) => {
     currentCursorMode.current = "default";
-    if (currentCursorMode.current === "default") {
+    if (!mobileViewportIsActive && currentCursorMode.current === "default") {
       cursorRef.current.style.height = "32px";
       cursorRef.current.style.width = "32px";
       cursorRef.current.style.mixBlendMode = "normal";
